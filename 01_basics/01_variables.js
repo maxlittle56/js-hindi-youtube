02d1f53f8a3ff5451b6c80697d18e7b4a46da180
maxlittle56 <75158264+maxlittle56@users.noreply.github.com>
2024-04-24 16:12:29 +0000

    variables in javascript

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,17 @@
+const accountId = 187778;
+let accountEmail = 'krishan32@yahoo.com';
+var accountPassword = 'krish1234';
+accountCity = 'Kathmandu';
+
+let accountState;
+
+// accountId = 2; //not allowed
+accountEmail = 'melon1000@gmail.com';
+accountPassword = '09111689';
+accountCity = 'Lalitpur';
+
+/*
+prefer not to use var
+*/
+
+console.table([accountId, accountEmail, accountPassword, accountCity,accountState]);
