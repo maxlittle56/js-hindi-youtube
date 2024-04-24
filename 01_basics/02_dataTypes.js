e4d8868bad55a269d6f1fc8931f2f8921f2f367e
maxlittle56 <75158264+maxlittle56@users.noreply.github.com>
2024-04-24 16:30:45 +0000

    datatypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,27 @@
+"use strict"; // treat all JS code as newer version
+
+// alert(3 + 3); // we are using nodejs, not browser
+
+console.log(5
+    +
+    10) // code readability should be high
+
+    console.log('Krishna');
+
+    let name = "Krishna";
+    let age = 18
+    let isLoggedIn = false
+
+    //number ==> 2 to power 53
+    // bigint
+    // string => ""
+    // boolean => true/false
+    // null => standalone value
+    // undefined
+    // symbol => unique
+
+
+    //object
+
+    console.log(typeof undefined); //undefined
+    console.log(typeof null); //object
