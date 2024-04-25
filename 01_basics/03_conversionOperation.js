90566e38404a5943ac2101496e36996bd8a0eaa3
maxlittle56 <75158264+maxlittle56@users.noreply.github.com>
2024-04-25 00:17:27 +0000

    conversion of data types

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,27 @@
+let score = "hitesh";
+
+// console.log(typeof score);
+
+let valueInNumber = Number(score);
+
+// console.log(valueInNumber, typeof valueInNumber);
+
+
+// "33" => 33
+// "33abc" => NaN
+// true => 1; false => 0
+
+
+let isLoggedIn = "";
+
+let booleanIsLoggedIn = Boolean(isLoggedIn);
+
+// console.log(booleanIsLoggedIn, typeof booleanIsLoggedIn);
+
+// 1 => true; 0 => false
+// "" => false
+// "krishna" => true
+
+let someNumber = 66;
+let stringNumber = String(someNumber);
+console.log(stringNumber, typeof stringNumber);
