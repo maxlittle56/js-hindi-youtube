8697d80e9cfa0b5fd0020c7c2b579b1df8533666
maxlittle56 <75158264+maxlittle56@users.noreply.github.com>
2024-04-25 00:43:26 +0000

    javascript operations

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -24,4 +24,41 @@ let booleanIsLoggedIn = Boolean(isLoggedIn);
 
 let someNumber = 66;
 let stringNumber = String(someNumber);
-console.log(stringNumber, typeof stringNumber);
+// console.log(stringNumber, typeof stringNumber);
+
+
+// ************************************ Operations **************************************
+
+let value = 3;
+let negValue = -value;
+// console.log(value, negValue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**4);
+// console.log(2/3);
+// console.log(2%3);
+
+let str1 = "hello";
+let str2 = "Krishna";
+
+let str3 = str1 + str2;
+// console.log(str3);
+
+// console.log("1" + 2);
+// console.log(1 + "2");
+// console.log("1" + 2 + 2);
+// console.log(2 + 2 + "1");
+
+// console.log(+true);
+// console.log(+"");
+
+let num1, num2, num3;
+
+num1 = num2 = num3 = 2 + 2;
+
+let gameCounter = 100;
+++gameCounter;
+
+console.log(gameCounter);
